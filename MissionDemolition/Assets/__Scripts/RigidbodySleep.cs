20bdc68c1541809f15c63a2816487585d38f9a43
zweatshirt <>
2024-02-18 00:19:43 -0600

    A little over halfway

---
+++ MissionDemolition/Assets/__Scripts/RigidbodySleep.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public class RigidbodySleep : MonoBehaviour { 
+
+    void Start () {
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb != null) rb.Sleep();
+        
+    }
+    
+}
